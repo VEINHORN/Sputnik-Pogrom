d105fb433b31ed527ef798dba5b4e8d3f473cded
VEINHORN <b.korogvich@gmail.com>
2015-07-16 02:40:08 +0300

    add admob

--- app/src/main/java/com/sputnikpogrom/ui/article/ArticleFragment.java
+++ app/src/main/java/com/sputnikpogrom/ui/article/ArticleFragment.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 import com.sputnikpogrom.loaders.ArticleLoader;
 
 import butterknife.Bind;
@@ -19,12 +21,17 @@ import veinhorn.sputnikpogrom.R;
  */
 public class ArticleFragment extends Fragment {
     @Bind(R.id.articleWebView) WebView articleWebView;
+    @Bind(R.id.adView) AdView adView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceS) {
         Activity activity = getActivity();
         View view = inflater.inflate(R.layout.fragment_article, container, false);
         ButterKnife.bind(this, view);
+
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
+
         new ArticleLoader(activity, articleWebView).execute(getArguments().getString("articleUrl"));
         return view;
     }
