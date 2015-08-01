7be2cfcf2c8d6e6e652f4192c8e039d1613a84ca
VEINHORN <b.korogvich@gmail.com>
2015-08-02 01:59:39 +0300

    add share button to article fragment

--- app/src/main/java/com/sputnikpogrom/ui/article/ArticleFragment.java
+++ app/src/main/java/com/sputnikpogrom/ui/article/ArticleFragment.java
@@ -4,9 +4,13 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
+import android.widget.Toast;
 
 import com.sputnikpogrom.loaders.ArticleLoader;
 
@@ -22,12 +26,30 @@ public class ArticleFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceS) {
+        setHasOptionsMenu(true);
         Activity activity = getActivity();
         View view = inflater.inflate(R.layout.fragment_article, container, false);
         ButterKnife.bind(this, view);
-
         new ArticleLoader(activity, articleWebView).execute(getArguments().getString("articleUrl"));
 
         return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        menu.clear();
+        inflater.inflate(R.menu.article_fragment_actions, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.action_share:
+                Toast.makeText(getActivity(), "test", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
